273ea3c27fce2b4820e4aac114793cfd202edf98
Marharyta Bahinska <marharyta_bahinska@epam.com>
2025-02-18 11:21:10 +0200

    task3

--- product-service/handlers/getProductById.js
+++ product-service/handlers/getProductById.js
@@ -6,10 +6,15 @@ exports.handler = async (event) => {
   try {
     const { productId } = event.pathParameters; // Extract productId from path parameters
     const product = mockProducts.find(p => p.id === productId);
+    const headers = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+      };
 
     if (!product) {
       return {
         statusCode: 404,
+        headers: headers,
         body: JSON.stringify({
           message: `Product with ID ${productId} not found`
         })
@@ -18,6 +23,7 @@ exports.handler = async (event) => {
 
     return {
       statusCode: 200,
+      headers: headers,
       body: JSON.stringify({
         product
       })
@@ -25,6 +31,7 @@ exports.handler = async (event) => {
   } catch (error) {
     return {
       statusCode: 500,
+      headers: headers,
       body: JSON.stringify({
         message: 'Failed to retrieve product.',
         error: error.message
